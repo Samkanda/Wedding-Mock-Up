a310ef4052f2ceba246ea69524a110c27e6c4209
Navpreet Kanda <52259259+Samkanda@users.noreply.github.com>
2023-02-08 11:53:11 -0500

    Tailwind Setup
    - Basic Tailwind Setup

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#391C05',
+        'secondary': '#A99E93',
+        'third': '#982628'
+
+      }
+    },
+  },
+  plugins: [],
+}
