2ed895835053b6d61fc4cc6d0efb8a44d7c412e6
Navpreet Kanda <52259259+Samkanda@users.noreply.github.com>
2023-02-13 13:25:04 -0500

    Update App.js
    - Connected all made components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import './App.css';
 import Nav from './components/Nav';
 import Main from './components/Main';
 import Services from './components/Services';
+import Gallery from './components/Gallery';
+import About from './components/About';
+import Social from './components/Social';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -9,6 +13,11 @@ function App() {
       <Nav/>
       <Main/>
       <Services/>
+      <Gallery/>
+      <About/>
+      <Social/>
+      <Contact/>
+      <Footer/>
     </div>
   );
 }
