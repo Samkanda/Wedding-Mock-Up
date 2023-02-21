0f3cd8886459e94f3cea38d9f74e2e3d3b44bb28
Navpreet Kanda <52259259+Samkanda@users.noreply.github.com>
2023-02-21 14:46:06 -0500

    Create Social.js
    - Created gallery that displays previous client photos
    - Responsive layout with images retrieved online
    - Hover effect that changes image from grayscale to color

---
+++ src/components/Social.js
@@ -0,0 +1,64 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div>
+    <div className="socialContainer flex justify-center mt-12">
+    <div className="line-container mt-6 sm:mt-8  bg-primary  w-11/12 h-px"/>
+    
+    <div className="socialText">
+        <h1 className=" bg-[#FFFAF0] sm: w-80  lg:w-96  socialTitle text-5xl md:text-5xl lg:text-6xl text-primary px-0 flex justify-center text-center">
+           On Instagram
+        </h1>
+        <div className=" mt-1 mb-5 sm:text-sm md:text-2xl text-secondary" >
+            <h2 className="flex justify-center"> Follow me at @weddingsaregreat</h2>
+        </div>
+    </div>
+</div>
+<div className="grid-container">
+  <div>
+    <img className='grid-item grid-item-1' src='https://images.unsplash.com/photo-1606800052052-a08af7148866?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' alt=''/>
+    <p>"Best day of my life!"</p>
+  </div>
+  <div>
+    <img className='grid-item grid-item-2' src='https://images.unsplash.com/photo-1546032996-6dfacbacbf3f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2024&q=80' alt=''/>
+    <p>"Love is everything"</p>
+  </div>
+  <div>
+    <img className='grid-item grid-item-3' src='https://images.unsplash.com/photo-1583939003579-730e3918a45a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80' alt=''/>
+    <p>"I love you so much!"</p>
+  </div>
+  <div>
+    <img className='grid-item grid-item-4' src='https://images.unsplash.com/photo-1494955870715-979ca4f13bf0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' alt=''/>
+    <p>"Forever, day and night"</p>
+  </div>
+  <div>
+    <img className='grid-item grid-item-5' src='https://images.unsplash.com/photo-1525772764200-be829a350797?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80' alt=''/>
+    <p>"My dreams came to life"</p>
+  </div>
+  <div>
+    <img className='grid-item grid-item-6' src='https://images.unsplash.com/photo-1519741497674-611481863552?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' alt=''/>
+    <p>"My best friend"</p>
+  </div>
+  <div>
+    <img className='grid-item grid-item-7' src='https://images.unsplash.com/photo-1523438885200-e635ba2c371e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80' alt=''/>
+    <p>"The scene was perfect"</p>
+  </div>
+  <div>
+    <img className='grid-item grid-item-8' src='https://images.unsplash.com/photo-1522673607200-164d1b6ce486?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' alt=''/>
+    <p>"Exactly what I wanted"</p>
+  </div>
+  <div>
+    <img className='grid-item grid-item-9' src='https://images.unsplash.com/photo-1544078751-58fee2d8a03b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' alt=''/>
+    <p>"Our adventure will never end"</p>
+  </div>
+  <div>
+    <img className='grid-item grid-item-10' src='https://images.unsplash.com/photo-1544592732-83bbbfc46783?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80' alt=''/>
+    <p>"I will never let go"</p>
+  </div>
+</div>
+</div>
+  )
+}
+
+export default Social
